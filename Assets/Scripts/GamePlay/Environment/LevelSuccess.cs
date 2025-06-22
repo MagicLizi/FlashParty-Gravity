39cad9c5308bcceb0afa6ad0964e37f2dd30e8db
Lizi <jiali@magiclizi.com>
2025-06-22 13:49:59 +0800

    动画

--- Assets/Scripts/GamePlay/Environment/LevelSuccess.cs
+++ Assets/Scripts/GamePlay/Environment/LevelSuccess.cs
@@ -5,7 +5,19 @@ using UnityEngine;
 public class LevelSuccess : MonoBehaviour
 {
     public List<GameObject> List = new List<GameObject>();
-    
+
+    private void Start()
+    {
+        // transform.localScale = Vector3.zero;
+        // GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+
+        // transform.DOScale(Vector3.one, 1f).SetEase(Ease.OutBack);
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = new Color(1, 1, 1, 0);
+        GetComponent<SpriteRenderer>().DOFade(1f, 0.5f).SetEase(Ease.OutQuad);
+
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         InputManager.Instance.Enable(false);
